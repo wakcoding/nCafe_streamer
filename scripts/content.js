227d5be224e62a15b7d5a9b0c8f4fe5574ae8cdd
cham_why <53082717+chamwhy@users.noreply.github.com>
2023-05-03 01:04:07 +0900

    댓글작성란으로 가기 구현 완료

--- scripts/content.js
+++ scripts/content.js
@@ -21,50 +21,119 @@ const observerYoutubeEmbed = new MutationObserver(function(mutationsList) {
 
 const observerArticle = new MutationObserver(function(mutationsList) {
     for (let mutation of mutationsList) {
-        if (mutation.type === 'subtree') {
+        if (mutation.type === 'childList') {
             const addedNodes = mutation.addedNodes;
             for (let node of addedNodes) {
                 console.log("article node changed");
-                // if (node.tagName === 'iframe' && node.id == "cafe_main") {
-                    observerYoutubeEmbed.observe(document.querySelector('.se-module.se-module-oembed'), { childList: true, subtree: true });
-               // }
+                if (node.className == "article_wrap") {
+                    console.log("it's work!!!!!!!!!!!!!!!");
+                    const iframeDoc = document.querySelector('iframe#cafe_main').contentWindow.document;
+                    console.log(iframeDoc.querySelector('.comment_tab'));
+                }
             }
         }
     }
 });
 
-observerArticle.observe(document.querySelector('iframe#cafe_main'), { childList: true, subtree: true });
 */
+const targetClass = 'comment_tab';
+
+const observer = new MutationObserver(function(mutationsList) {
+    const iframeDoc = document.querySelector('iframe#cafe_main').contentWindow.document;
+    for (let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            for (let addedNode of mutation.addedNodes) {
+                if (addedNode.classList && addedNode.classList.contains(targetClass)) {
+                    console.log(addedNode);
+                    if(addedNode){
+                        function MoveToCommentWriter(){
+                            const location = iframeDoc.querySelector(".CommentWriter").offsetTop;
+                            window.scrollTo({top:location, behavior:'smooth'});
+                        }
+                        console.log("There is commentTab");
+                    
+                        addedNode.innerHTML += `
+                        <button class="nCafe-Btn nCafe-GrayBtn nCafe-MoveToCommentWriter">
+                            댓글쓰기
+                        </button>
+                        `;
+                        const moveToCommentWriterBtn = iframeDoc.querySelector('.nCafe-MoveToCommentWriter');
+                        moveToCommentWriterBtn.addEventListener('click', ()=>{
+                            MoveToCommentWriter();
+                        });
+                    }else{
+                        console.log("There is no commentTab");
+                    }
+                }
+            }
+        }
+    }
+});
+
+const config = { childList: true, subtree: true };
+
+
+
 
 document.querySelector('iframe#cafe_main').onload = function(){
     const iframeDoc = document.querySelector('iframe#cafe_main').contentWindow.document;
-    console.log(iframeDoc.querySelector('.layout_content'));
-    iframeDoc.querySelector('.layout_content').onload = function(){
-        console.log(iframeDoc.querySelector('.ArticleContentBox'));
-    };
-    
+    if(iframeDoc.querySelector('body')){
+        observer.observe(iframeDoc.querySelector('body'), { childList: true, subtree: true });
+    }
 };
+    
 
+/*
+const iframeMain = document.querySelector('iframe#cafe_main');
+console.log(iframeMain);
+iframeMain.addEventListener('load', ()=>{
+    const iframeDoc = iframeMain.contentWindow.document;
+    console.log(iframeDoc.querySelector('.Article.layout_content'));
+    iframeDoc.querySelector('.Article.layout_content').addEventListener('load', ()=>{
+        const commentTab = iframeDoc.querySelector('.comment_tab');
+        console.log(commentTab);
+        if(commentTab){
+            console.log("There is commentTab");
+        
+            commentTab.innerHTML += `
+            <button class="nCafe-Btn nCafe-GrayBtn nCafe-MoveToCommentWriter">
+                댓글쓰기
+            </button>
+            `;
+            const moveToCommentWriterBtn = iframeDoc.querySelector('.nCafe-MoveToCommentWriter');
+            moveToCommentWriterBtn.addEventListener('click', ()=>{
+            MoveToCommentWriter();
+            });
+        }else{
+            console.log("There is no commentTab");
+        }
 
-const commentTab = document.querySelector('.comment_tab');
-if(commentTab){
-    console.log("There is commentTab");
-  
-    commentTab.innerHTML += `
-    <button class="nCafe-Btn nCafe-GrayBtn nCafe-MoveToCommentWriter">
-        댓글쓰기
-    </button>
-    `;
-    const moveToCommentWriterBtn = document.querySelector('.nCafe-MoveToCommentWriter');
-    moveToCommentWriterBtn.addEventListener('click', ()=>{
-      MoveToCommentWriter();
+        function MoveToCommentWriter(){
+            const location = iframeDoc.querySelector(".CommentWriter").offsetTop;
+            window.scrollTo({top:location, behavior:'smooth'});
+        }
     });
-}else{
-    console.log("There is no commentTab");
-}
+});
 
-function MoveToCommentWriter(){
-    const location = document.querySelector(".CommentWriter").offsetTop;
-    window.scrollTo({top:location, behavior:'smooth'});
-}
+const observer = new MutationObserver(function(mutationsList) {
+    for(let mutation of mutationsList) {
+      if (mutation.type === 'childList') {
+        for (let addedNode of mutation.addedNodes) {
+          if (addedNode.tagName === 'IFRAME') {
+            addedNode.addEventListener('load', function() {
+              // Code to execute when the iframe has finished loading
+              const iframeElement = addedNode.contentWindow.document.getElementById('my-iframe-element');
+              // Code to execute when the iframe element has changed
+            });
+          }
+        }
+      }
+    }
+  });
+  
+  const targetNode = document.getElementById('target-element');
+  const config = { childList: true, subtree: true };
+  
+  observer.observe(targetNode, config);
 
+  */
