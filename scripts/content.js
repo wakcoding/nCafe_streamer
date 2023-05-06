ab0b0907c000dba5821db06353c887eba82137d0
cham_why <53082717+chamwhy@users.noreply.github.com>
2023-05-07 00:17:18 +0900

    content js to codes obj

--- scripts/content.js
+++ scripts/content.js
@@ -7,6 +7,7 @@ const targetClass = 'comment_tab';
 const codes = {};
 
 codes.commentWriterTargetClass = 'comment_tab';
+codes.noticeTargetClass = 'unload';
 
 codes.commentWriterObserver = new MutationObserver(function(mutationsList) {
     const iframeDoc = document.querySelector('iframe#cafe_main').contentWindow.document;
@@ -47,24 +48,9 @@ codes.noticeObserver = new MutationObserver(function(mutationsList) {
     for (let mutation of mutationsList) {
         if (mutation.type === 'childList') {
             for (let addedNode of mutation.addedNodes) {
-                if (addedNode.classList && addedNode.classList.contains(codes.commentWriterTargetClass)) {
+                if (addedNode.classList && addedNode.classList.contains(codes.noticeTargetClass)) {
                     if(addedNode){
-                        function MoveToCommentWriter(){
-                            const location = iframeDoc.querySelector(".CommentWriter").offsetTop;
-                            if(!location) console.log("There is no commentWriter.");
-                            window.scrollTo({top:location, behavior:'smooth'});
-                        }
-                    
-                        addedNode.innerHTML += `
-                        <button id="moveToCW">
-                            댓글쓰기
-                        </button>
-                        `;
-                        const moveToCommentWriterBtn = iframeDoc.querySelector('#moveToCW');
-                        moveToCommentWriterBtn.classList.add("nCafe-Btn", "nCafe-GrayBtn", "nCafe-MoveToCommentWriter");
-                        moveToCommentWriterBtn.addEventListener('click', ()=>{
-                            MoveToCommentWriter();
-                        });
+                        console.log(addedNode.onclick);
                     }else{
                         console.log("There is no commentTab");
                     }
@@ -83,19 +69,13 @@ codes.init = ()=> {
     cafeMain.onload = function(){
         const iframeDoc = document.querySelector('iframe#cafe_main').contentWindow.document;
 
-        const 
-        const noticeArticles = iframeDoc.querySelectorAll('._noticeArticle>.td_article');
+        const noticeArticles = iframeDoc.querySelector('._noticeArticle>.td_article');
         if(iframeDoc.querySelector('body')){
-            
+            console.log('body had started');
             codes.commentWriterObserver.observe(iframeDoc.querySelector('body'), { childList: true, subtree: true });
+            codes.noticeObserver.observe(iframeDoc.querySelector('body'), { childList: true, subtree: true });
         }
-        if(iframeDoc.querySelector('body')){
-            
-            codes.commentWriterObserver.observe(iframeDoc.querySelector('body'), { childList: true, subtree: true });
-        }
-        if(noticeArticles){
-            codes.noticeObserver.observe()
-        }
+        
     };
 };
 
@@ -103,3 +83,4 @@ codes.init = ()=> {
 
 
 
+codes.init();
