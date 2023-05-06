f8df11f6441db9b333b06d405306781676e54a0c
cham_why <53082717+chamwhy@users.noreply.github.com>
2023-05-07 01:16:13 +0900

    add webtoon viewer button

--- scripts/content.js
+++ scripts/content.js
@@ -8,8 +8,9 @@ const codes = {};
 
 codes.commentWriterTargetClass = 'comment_tab';
 codes.noticeTargetClass = 'unload';
+codes.webtoonViewerClass = 'article_wrap';
 
-codes.commentWriterObserver = new MutationObserver(function(mutationsList) {
+codes.addedObserver = new MutationObserver(function(mutationsList) {
     const iframeDoc = document.querySelector('iframe#cafe_main').contentWindow.document;
     if(!iframeDoc) return;
     for (let mutation of mutationsList) {
@@ -24,12 +25,12 @@ codes.commentWriterObserver = new MutationObserver(function(mutationsList) {
                         }
                     
                         addedNode.innerHTML += `
-                        <button id="moveToCW">
+                        <button id="moveToCW" class="nCafe-Btn nCafe-GrayBtn nCafe-MoveToCommentWriter">
                             댓글쓰기
                         </button>
                         `;
                         const moveToCommentWriterBtn = iframeDoc.querySelector('#moveToCW');
-                        moveToCommentWriterBtn.classList.add("nCafe-Btn", "nCafe-GrayBtn", "nCafe-MoveToCommentWriter");
+                        // moveToCommentWriterBtn.classList.add("nCafe-Btn", "nCafe-GrayBtn", "nCafe-MoveToCommentWriter");
                         moveToCommentWriterBtn.addEventListener('click', ()=>{
                             MoveToCommentWriter();
                         });
@@ -37,28 +38,46 @@ codes.commentWriterObserver = new MutationObserver(function(mutationsList) {
                         console.log("There is no commentTab");
                     }
                 }
-            }
-        }
-    }
-});
 
-codes.noticeObserver = new MutationObserver(function(mutationsList) {
-    const iframeDoc = document.querySelector('iframe#cafe_main').contentWindow.document;
-    if(!iframeDoc) return;
-    for (let mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            for (let addedNode of mutation.addedNodes) {
-                if (addedNode.classList && addedNode.classList.contains(codes.noticeTargetClass)) {
+                if(addedNode.classList && addedNode.classList.contains(codes.noticeTargetClass)){
                     if(addedNode){
                         console.log(addedNode.onclick);
                     }else{
-                        console.log("There is no commentTab");
+                        console.log("There is no notice articles");
+                    }
+                }
+
+                if(addedNode.classList && addedNode.classList.contains(codes.webtoonViewerClass)){
+                    if(addedNode){
+                        const leftArea = addedNode.querySelector('.ArticleTopBtns>.left_area');
+                        if(!leftArea) return;
+                        leftArea.innerHTML += `
+                        <button id="webtoonViewerBtn" class="nCafe-Btn nCafe-GrayBtn nCafe-webtoonViewer">
+                            웹툰뷰어
+                        </button>
+                        `;
+                        const moveToCommentWriterBtn = iframeDoc.querySelector('#moveToCW');
+                        moveToCommentWriterBtn.addEventListener('click', ()=>{
+                            const groupArea =  document.querySelector('#group-area');
+                            const isWebtoonView = groupArea.classList.contains('webtoonView');
+                            if(isWebtoonView){
+                                groupArea.classList.remove('webtoonView');
+                                document.querySelector('#main-area').classList.remove('webtoonView');
+                            }else{
+                                groupArea.classList.add('webtoonView');
+                                document.querySelector('#main-area').classList.add('webtoonView');
+                            }
+                            
+                        });
+                    }else{
+                        console.log("There is no webtoonView");
                     }
                 }
             }
         }
     }
 });
+
 
 
 codes.init = ()=> {
@@ -72,10 +91,9 @@ codes.init = ()=> {
         const noticeArticles = iframeDoc.querySelector('._noticeArticle>.td_article');
         if(iframeDoc.querySelector('body')){
             console.log('body had started');
-            codes.commentWriterObserver.observe(iframeDoc.querySelector('body'), { childList: true, subtree: true });
-            codes.noticeObserver.observe(iframeDoc.querySelector('body'), { childList: true, subtree: true });
+            codes.addedObserver.observe(iframeDoc.querySelector('body'), { childList: true, subtree: true });
         }
-        
+        console.log(iframeDoc.querySelector('.Article').innerHTML);
     };
 };
 
